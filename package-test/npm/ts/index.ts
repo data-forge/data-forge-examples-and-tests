13c5a7a22a11aa4b4390a47674d91c1364719bed
Ashley Davis <ashley@codecapers.com.au>
2018-09-22 09:26:52 +1000

    Now have a package test for both JS and TS use cases of Data-Forge.

---
+++ package-test/npm/ts/index.ts
@@ -0,0 +1,27 @@
+import * as dataForge from 'data-forge-beta';
+import 'data-forge-fs';
+
+// 
+// Create a simple data frame.
+//
+var values = dataForge
+	.range(0, 14)
+    .select(i => [i, Math.sin(i), Math.cos(i)]);
+    
+var dataFrame = new dataForge.DataFrame({
+		columnNames: ["index", "Sin", "Cos"], 
+		rows: values
+	})
+	.setIndex("index")
+    .dropSeries("index");
+    
+console.log(dataFrame.skip(4).take(5).toString());
+
+var series = dataFrame.getSeries("Sin");
+console.log(series.skip(4).take(5).toString());
+
+dataFrame.asCSV().writeFileSync("./test.csv");
+
+const df2 = dataForge.readFileSync("./test.csv").parseCSV();
+console.log("After save and load:");
+console.log(df2.toString());
